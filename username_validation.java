90f8c0774864a1ea84e3f4ee63c76ea32f2e1e1c
naim <thanvirdx919@gmail.com>
2019-04-03 01:23:36 -0400

    added special characters method to verify user validaiton

--- username_validation.java
+++ username_validation.java
@@ -1,6 +1,10 @@
+package username_implement;
+
 import java.util.Scanner;
+
 public class username_validation {
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		System.out.println("Enter a username with following crititer.\n"
 				+ "-between 8 to 30 characters\n-starts with an aplhabet\n"
 				+ "-does not contain special characters exception" +"_");
@@ -21,14 +25,35 @@ public class username_validation {
 		char startAlpha=userToArray[0];
 		
 		if(s.length()<8 || s.length()>31) {
-			System.out.print(s+ " is not between 8 to 30 characters");
+			if(s.length()<8){
+			   System.out.print(s+ " is less than 8 characters\n");
+			} else {
+			   System.out.print(s+ " is greater than 30 characters\n");
+			}
 		}
 		else if (!Character.isAlphabetic(startAlpha)) {
-			System.out.print("Username fails to start with a letter");
+			System.out.print("Username fails to start with a letter\n");
+		}
+		else if (hasSpecial(s)) {
+			System.out.print("Username cannot contain special characters\n");
 		}
 		else {
-			System.out.print("valid username");
+			System.out.print("valid username\n");
 		}
+		
+	}
+	
+	public static boolean hasSpecial(String input) {
+		int specials=0;
+		
+		for(int i=0; i<input.length();i++) {
+			char ch=input.charAt(i);
+			if(!Character.isDigit(ch) && !Character.isLetter(ch)) {
+				specials++;
+			}
+		}
+		
+		return specials>0;
 	}
 
-}
+	}
